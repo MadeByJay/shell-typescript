8bfc61a072830f79efcfa8b0933cdae2f3053479
Johnny B <jrbostonjr@gmail.com>
2025-11-30 17:03:52 -0500

    fix test failure

--- app/main.ts
+++ app/main.ts
@@ -7,5 +7,5 @@ const rl = createInterface({
 
 // TODO: Uncomment the code below to pass the first stage
 rl.question("$ ", (command) => {
-  console.log(command);
+  console.log(`${command}: command not found`);
 });
